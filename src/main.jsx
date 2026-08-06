5c980ba711d565f83cec9e63168b0332b6072ace
Marcos Uteich <42223606+MarcosUteich@users.noreply.github.com>
2026-08-05 23:33:23 -0300

    test: importa broken-test.ts no main.jsx para forçar erro de build

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './broken-test.ts' // Import intencional de arquivo com erro
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
