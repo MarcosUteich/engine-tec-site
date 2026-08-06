ee6cc2ca38ee54dcc6b7c356b2f6e9de22047c01
Marcos Uteich <42223606+MarcosUteich@users.noreply.github.com>
2026-08-05 23:35:29 -0300

    fix: remove import do arquivo de teste

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './broken-test.ts' // Import intencional de arquivo com erro
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
